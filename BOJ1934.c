94c6a77a467474855e343c773a35dfd34cced6cc
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2021-09-24 20:45:55 +0900

    이해 안 됨 / 다시 한 번 풀어볼 것
    https://www.acmicpc.net/problem/1934

---
+++ BOJ1934.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int gcd(int A, int B) {
+	if (B == 0)
+		return A;
+	else
+		return gcd(B, A % B);
+}
+
+int main(void) {
+	int T;
+	scanf_s("%d", &T);
+	int A; int B;
+
+	for (int i = 0; i < T; i++) {
+		scanf_s("%d %d", &A, &B);
+		printf("%d", A * B / gcd(A, B));
+	}
+}
+
