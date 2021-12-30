38ce7b3fadabf3fb71941ee39f57a886a6ee339c
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2021-12-30 18:32:47 +0900

    A + B - 8

---
+++ 2021.12/BOJ11022.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct numbers {
+	int a;
+	int b;
+	int sum;
+}numbers;
+
+int main(void) {
+
+	int t;
+	scanf("%d", &t);
+
+	numbers number[1000];
+	for (int i = 1; i <= t; i++) {
+		scanf("%d %d", &number[i].a, &number[i].b);
+		number[i].sum = number[i].a + number[i].b;
+	}
+	for (int i = 1; i <= t; i++) {
+
+		printf("Case #%d: %d + %d = %d\n", i, number[i].a, number[i].b, number[i].sum);
+	}
+
+	return 0;
+}
