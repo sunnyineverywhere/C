aadf69f92a218160ebc1c1bdd5058132f04f5da7
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2022-01-04 01:45:05 +0900

    구구단

---
+++ 2022.01/BOJ2743.c
@@ -0,0 +1,14 @@
+/* 백준 2739번 구구단 */
+
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= 9; i++) {
+		printf("%d * %d = %d\n", n, i, n * i);
+	}
+
+	return 0;
+}
